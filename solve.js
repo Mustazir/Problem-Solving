8f1f1656f958723b7691993a7102b50308749cd9
Mustazir Billah <billah15-5193@diu.edu.bd>
2025-06-11 13:56:38 +0600

    Problem 8 SOlve

--- solve.js
+++ solve.js
@@ -78,3 +78,24 @@ function isAnagram(str1, str2) {
 // Example Usage
 console.log(isAnagram("listen", "silent")); // true
 console.log(isAnagram("hello", "world")); // false
+
+
+
+// problem 8
+
+function fizzBuzz() {
+  for (let i = 1; i <= 100; i++) {
+    if (i % 15 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+  }
+}
+
+// Example Usage
+fizzBuzz();
