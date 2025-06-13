2a3e749a15189262cb0ed26bf4dbfdd5284ce41b
mustazir <billah15-5193@diu.edu.bd>
2025-06-13 23:11:00 +0600

    Problem 11 solved

--- solve.js
+++ solve.js
@@ -122,3 +122,37 @@ function customMap(array, callback) {
 
 
 console.log(customMap([1, 2, 3], x => x * 2)); // [2, 4, 6]
+
+// Problem 11
+
+function deepEqual(obj1, obj2) {
+ 
+  if (obj1 === obj2) return true;
+
+
+  if (typeof obj1 !== "object" || typeof obj2 !== "object" || obj1 === null || obj2 === null) {
+    return false;
+  }
+
+ 
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
+
+
+  if (keys1.length !== keys2.length) return false;
+
+
+  for (let key of keys1) {
+    if (!keys2.includes(key) || !deepEqual(obj1[key], obj2[key])) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+
+console.log(deepEqual({ a: 1, b: 2 }, { a: 1, b: 2 })); // true
+console.log(deepEqual({ a: 1, b: 2 }, { a: 1, b: 3 })); // false
+console.log(deepEqual({ a: 1, b: { c: 2 } }, { a: 1, b: { c: 2 } })); // true
+console.log(deepEqual({ a: 1 }, { a: 1, b: 2 })); // false
